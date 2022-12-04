9a16122adc06f9e209609a6dfcd0c964e9f0eaf0
indexshare <indexshare0@gmail.com>
2022-12-04 12:37:39 +0530

    2

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import schedule, time, datetime
+import schedule, time, datetime, requests
 from threading import Timer
 
+def send_telegram_message(request):
+    bot_token = '5969891290:AAE13zPtwdc2P3VqZy6o_7opvRHbAtH_vfE'
+    bot_chatID = '998029180'
+    message = request
+    # message = datetime.datetime.now()
+    apiURL = f'https://api.telegram.org/bot{bot_token}/sendMessage'
 
-def my_job():
-    print ("hello world")
-nextDay = datetime.datetime.now() + datetime.timedelta(days=1)
-dateString = datetime.datetime.now().strftime('%d-%m-%Y') + " 11-25-00"
-newDate = nextDay.strptime(dateString,'%d-%m-%Y %H-%M-%S')
-delay = (newDate - datetime.datetime.now()).total_seconds()
+    try:
+        response = requests.post(apiURL, json={'chat_id': bot_chatID, 'text': message})
+        # print(response.text)
+    except Exception as e:
+        print(e)
+    return
+
+
+# nextDay = datetime.datetime.now() + datetime.timedelta(days=1)
+# dateString = datetime.datetime.now().strftime('%d-%m-%Y') + " 11-25-00"
+# newDate = nextDay.strptime(dateString,'%d-%m-%Y %H-%M-%S')
+
+currentTime = datetime.datetime.now()
+executionTime = datetime.datetime.now().strftime('%d-%m-%Y') + " 12-50-00"
+executionTime = datetime.datetime.strptime(executionTime, '%d-%m-%Y %H-%M-%S')
+delay = (executionTime - currentTime).total_seconds()
 print(delay)
-# Timer(str(delay),my_job,()).start()
-t = Timer(10.0, my_job)
-t.start()
+Timer(10.0, send_telegram_message, [str(datetime.datetime.now())]).start()
+Timer(30.0, send_telegram_message, [str(datetime.datetime.now())]).start()
+Timer(delay, send_telegram_message, [str(datetime.datetime.now())]).start()
 
 # def job():
 #     print("I am doing this job!")
