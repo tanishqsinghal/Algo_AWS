2f9fbb7dc3c479a612d101612ccd97a8ee0cbde4
indexshare <indexshare0@gmail.com>
2023-09-11 13:40:01 +0530

    13

--- config_file.py
+++ config_file.py
@@ -12,8 +12,8 @@ config = {
     "target": 0,
     "trades_exited": False,
     "message_to_send": "",
-    "scanner_start_time": datetime.now(),
-    "scanner_end_time": datetime.now()
+    "scanner_start_time": datetime.datetime.now(),
+    "scanner_end_time": datetime.datetime.now()
 }
 
 def send_telegram_message(request):
